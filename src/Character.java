6ce887b8d28d7232de0267046e4f710f273d9c17
Max Kurilov <maxkurilov9@gmail.com>
2026-05-13 22:38:07 -0700

    Add files via upload

---
+++ src/Character.java
@@ -0,0 +1,86 @@
+public class Character {
+    private String name;
+    private int hp;
+    private int damage;
+    private int speed;
+    private int position;
+
+
+    public Character(){
+        this("someone", 10, 5, 10);
+    }
+
+    public Character(String name, int hp, int damage, int speed){
+        this.name = name;
+        this.hp = hp;
+        this.damage = damage;
+        this.speed = speed;
+        position = 0;
+    }
+
+
+    public void attack(Character ch){
+        ch.increaseHp(getDamage());
+    }
+
+    public void increaseHp(int dmg){
+        hp -= dmg;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void addHp(int hp){
+        this.hp += hp;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void addDamage(int damage){
+        this.damage += damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void addSpeed(int speed){
+        this.speed += speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+
+    public void addPosition(int pos){
+
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
